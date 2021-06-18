995c8b6ecae89f7e8328cae764e50a7baa605a1c
Junior030 <juniorhenrique030@gmail.com>
2021-06-18 17:26:29 -0300

    Fim 100%

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import Header from '../components/Header';
+import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
+import { Link } from 'react-router-dom';
 
 class Ranking extends React.Component {
   constructor(props) {
@@ -7,16 +9,46 @@ class Ranking extends React.Component {
     this.state = { };
   }
 
+  renderRanking() {
+    const ranking = JSON.parse(localStorage.getItem('ranking')) || [];
+    const UM_NEGATIVE = -1;
+    const ordenadoRanking = ranking
+      .sort((a, b) => ((a.playerScore > b.playerScore) ? UM_NEGATIVE : 1));
+    return (
+      <ul>
+        {
+          ordenadoRanking.map((player, index) => (
+            <ol key={ index }>
+              <img
+                src={ `https://www.gravatar.com/avatar/${md5(player.playerGravatar).toString()}` }
+                alt="Avatar do jogador"
+              />
+              <span data-testid={ `player-name-${index}` }>{player.playerName}</span>
+              <span data-testid={ `player-score-${index}` }>{player.playerScore}</span>
+              <span>{player.assertions}</span>
+            </ol>
+          ))
+        }
+      </ul>
+    );
+  }
+
   render() {
     return (
       <div>
-        <Header />
         <section data-testid="ranking-title">
-          <p data-testid="feedback-total-score">Score</p>
+          {this.renderRanking()}
+          <Link to="/">
+            <button type="button" data-testid="btn-go-home">Home</button>
+          </Link>
         </section>
       </div>
     );
   }
 }
 
-export default Ranking;
+const mapStateToProps = (state) => ({
+  playerGravatar: state.playerReducer.playerGravatar,
+});
+
+export default connect(mapStateToProps)(Ranking);
