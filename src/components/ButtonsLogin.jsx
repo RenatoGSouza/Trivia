7fb929b439578dc5a7d32db923c6ca34b8596d5b
renatogsouza <renatomacedo1995@gmail.com>
2021-06-22 21:37:35 -0300

    separando as partes do login por components e adicioando css

---
+++ src/components/ButtonsLogin.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class ButtonsLogin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+  render() {
+    const { clickBtnJogar, validForm, redirect } = this.props;
+    return (
+      <div className="div-login">
+        <button
+          className="button-login"
+          type="button"
+          data-testid="btn-settings"
+          onClick={ () => redirect() }
+        >
+          Configurações
+        </button>
+        <Link to="/jogar">
+          <button
+            className="button-login"
+            type="submit"
+            data-testid="btn-play"
+            disabled={ validForm }
+            onClick={ clickBtnJogar }
+          >
+            Jogar
+          </button>
+        </Link>
+      </div>
+    );
+  }
+}
+
+ButtonsLogin.propTypes = {
+  clickBtnJogar: PropTypes.func.isRequired,
+  validForm: PropTypes.string.isRequired,
+  redirect: PropTypes.func.isRequired,
+};
+
+export default ButtonsLogin;
---
+++ src/components/ImgLogin.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ImgLogin extends Component {
+  render() {
+    return (
+      <div className="img-login">
+        <h1 className="h1-login">
+          TRIVIA
+          GAME
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default ImgLogin;
